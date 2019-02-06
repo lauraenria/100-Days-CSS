c193243040e8e9b55e9ef6d667485c994646895c
Laura Enria <enria.laura2@gmail.com>
2019-02-06 15:47:58 +0000

    style: finish style challenge n33

---
+++ n032/app/operations.js
@@ -0,0 +1,10 @@
+const btnMinus = document.querySelector(".btn-minus");
+const btnPlus = document.querySelector(".btn-plus");
+const num = document.querySelector(".number");
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+const clickEvent = a => {
+  addEventListener;
+};
